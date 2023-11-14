e8abbffd7489b7a180691f32103c5808e9ef706a
Rosen Koychev <rosen.koichev@gmail.com>
2023-11-14 02:58:57 +0200

    Modified the requester

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,20 +1,13 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 const base_url = "http://localhost:3030/jsonstore/games";
 
 export const getAll = async () => {
-    const result = await request("GET", base_url);
+    const result = await request.get(base_url);
     console.log(result);
     return Object.values(result);
 };
 
 export const create = async (gameData) => {
-    const response = await fetch(`${base_url}`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify(gameData),
-    });
-    const result = await response.json();
-    return result;
+    const result = await request.post(base_url, gameData);
+    return result;  
 };
