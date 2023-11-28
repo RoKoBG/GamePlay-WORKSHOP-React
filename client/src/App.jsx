199c36ce3f670faad93aa1ef2b009adf6dec0454
Rosen Koychev <95128567+RoKoBG@users.noreply.github.com>
2023-11-28 13:08:26 +0200

    Add auth service
    Add login redirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import * as authService from "./services/authService";
+import AuthContext from "./contexts/authContext";
+import Path from "./path";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GameList from "./components/game-list/GameList";
@@ -6,35 +11,33 @@ import GameCreate from "./components/game-create/GameCreate";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import GameDetails from "./components/game-details/GameDetails";
-import { useState } from "react";
-import AuthContext from "./contexts/authContext";
 
 function App() {
-    const [auth, setAuth] = useState({});
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState({});
 
-    const loginSubmitHandler = (values) => {
-        setAuth(values);
-        console.log(values);
-    };
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
 
-    return (
-        <AuthContext.Provider value={{ loginSubmitHandler }}>
-            <div id="box">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/games" element={<GameList />} />
-                    <Route path="/create" element={<GameCreate />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                    <Route
-                        path="/games/:gameId/details"
-                        element={<GameDetails />}
-                    />
-                </Routes>
-            </div>
-        </AuthContext.Provider>
-    );
+    setAuth(result);
+    navigate(Path.Home);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
+      <div id="box">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/create" element={<GameCreate />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="/games/:gameId/details" element={<GameDetails />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
